a5334e7b7fbe2d88ab93d88a32b10c35f9cd3b99
chidiebere onyedinma <codebreakersmd@gmail.com>
2019-04-15 17:36:09 -0400

    Improved Printing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        printStatus();
+    }
+
+    public static void printStatus(){
         System.out.println("Hello World!");
     }
 }
+
